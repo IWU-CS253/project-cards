633c4c9c2a226ba504aae03a7765c097ea54f4dd
irktorz <etorres@iwu.edu>
2022-10-29 11:47:42 -0500

    added some potential tablesand more attributes for the card table.

--- database.sql
+++ database.sql
@@ -3,4 +3,14 @@ CREATE TABLE cards (
     name VARCHAR(20),
     rank INT,
     type VARCHAR(5),
+    pack VARCHAR(10),
+    amount INT
+);
+
+CREATE TABLE collection (
+
+);
+
+CREATE TABLE store (
+
 );
