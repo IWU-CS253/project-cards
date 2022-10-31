da351a2eacb36e4e3e4f9deb118ce9720de18a27
SavitarS3 <113626178+SavitarS3@users.noreply.github.com>
2022-10-30 22:54:06 -0500

    Added app.py with routes to links that were added in home.html

---
+++ app.py
@@ -0,0 +1,70 @@
+import os
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, g, redirect, url_for, render_template, flash
+
+app = Flask(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('database.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/')
+def show_entries():
+    return render_template('home.html')
+
+
+@app.route('/your_inventory')
+def your_inventory():
+    return render_template('user_collection.html')
+
+
+@app.route('/marketplace')
+def marketplace():
+    return render_template('marketplace.html')
+
+
+@app.route('/connect_with_friends')
+def connect_with_friends():
+    return render_template('friends.html')
